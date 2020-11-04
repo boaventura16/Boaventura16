7c7b662ef55530d469e8cd2248437761b3f0938d
boaventura16 <73803612+boaventura16@users.noreply.github.com>
2020-11-04 15:09:39 -0300

    Update #Desafio 013.py

--- Estudos/#Desafio 013.py
+++ Estudos/#Desafio 013.py
@@ -1,4 +1,5 @@
 #Desafio 013
+#Aumento salarial
 s = int(input('Salario atual: '))
 a = s+(s*15/100)
-print('Salario atual: {}\nNovo salario: {}'.format(s, a))
+print('Salario atual: {}\nNovo salario com reajuste de 15%: {}'.format(s, a))
