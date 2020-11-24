a9279c2fbfb0168bcde093d47be4f25d5a1740d0
boaventura16 <boaventura0616@gmail.com>
2020-11-23 22:17:27 -0300

    Sequência de Fibonacci
    exercicio, sequência de Fibonacci.

--- Estudos/Desafio014.py
+++ Estudos/Desafio014.py
@@ -1,1 +1,16 @@
-print('Ola mundo!')
+print('-'*30)
+print('SEQUÊNCIA DE FIBONACCI')
+print('-'*30)
+n = int(input('Digite a quantidade de sequências: '))
+print('~')
+t1 = 0
+t2 = 1
+c = 0
+print('{} → {}'.format(t1, t2), end = '')
+while c <= n:
+    t3 = t1 + t2
+    c += 1
+    t1 = t2
+    t2 = t3
+    print(' → {}'.format(t3), end = '')
+print('→ FIM')
