334c4ff882df7e6147fabb74dd452905ffaf8d9b
Boaventura Lima <boaventura0616@gmail.com>
2020-12-15 08:03:23 -0300

    Dados de jogador em dicionarios com listas

--- Estudos/Desafio033.py
+++ Estudos/Desafio033.py
@@ -1,38 +1,44 @@
-dados = dict()
+dados = {}
 lista = list()
-s = media = 0
+gols = []
 while True:
   dados.clear()
-  dados['nome'] = input('Nome: ')
-  while True:
-    dados['sexo'] = input(f'Sexo de {dados["nome"]} [M/F]: ').upper()
-    if dados['sexo'] in 'MF':
-      break
-    print('ERRO! Opção invalida, digite M ou F.')
-  dados['idade'] = int(input(f'Idade de {dados["nome"]}: ')
+  dados['jogador'] = input('Nome: ')
+  r = int(input(f'Quantos jogos {dados["jogador"]} fez: '))
+  for c in range(1, r+1):
+    gols.append(int(input(f'Quantos gols na partida {c}: ')))
+  dados['gols'] = gols[:]
+  dados['total'] = sum(dados['gols'])
   lista.append(dados.copy())
-  s += dados['idade']
-  media = s / len(lista)
+  gols.clear()
   while True:
-    r = input('Quer continuar? [S/N]: ').upper()
-    if r in 'SN':
+    resposta = input('Quer continuar [S/N]: ').upper()
+    if resposta in 'SN':
       break
     print('ERRO! Opção invalida, digite S ou N.')
-  if r == 'N':
+  if resposta in 'N':
     break
-print('-=' *30)
-print(f'Foram cadastradas {len(lista)} pessoas.')
-print(f'A media de Idade das pessoas cadastradas é de {media} anos')
-print(f'As mulheres cadastradas foram: ', end='')
-for p in lista:
-  if p['sexo'] in 'F':
-    print(f'{p['nome']}', end='')
+print('-='*30)
+print(f'Cod', end=' ')
+for i in dados.keys():
+  print(f'{i:<15}', end=' ')
+print()
+print('-'*40)
+for k, v in enumerate(lista):
+  print(f'{k:>3}', end=' ')
+  for d in v.values():
+    print(f'{str(d):<15}', end=' ')
   print()
-print(f'As pessoas acima da média de idade são: ', end='')
-for p in lista:
-  if p['idade'] > media:
-    print('   ')
-    for v, k in p.items():
-      print(f'{v} = {k}', end='')
-    print()
-print('<< Encerrado >>')
+print('-'*40)
+while True:
+  busca = int(input('Quer mostrar dados de qual jogador? [999] para parar. '))
+  if busca == 999:
+    break
+  if busca > len(lista):
+    print('ERRO! Esse jogador não existe.')
+  else:
+    print(f' --- LEVANTAMETO DE JOGADOR {lista[busca]["jogador"]}: ')
+    for i, g in enumerate(lista[busca]['gols']):
+      print(f'   No jogo {i+1} fez {g} gols')
+  print('-'*40)
+print('<< VOLTE SEMPRE >>')
